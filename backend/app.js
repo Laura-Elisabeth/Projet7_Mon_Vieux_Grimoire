3fb6077609a9f1ef1f99745383060eb988c92464
Laura-Elisabeth <nevesdosreislauraelisabeth@ymail.com>
2024-01-12 17:18:23 +0100

    Book routes and controllers. Express Router.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Book = require('./models/book');
+
+const bookRoutes = require('./routes/books');
 
 mongoose.connect('mongodb+srv://Laura:NcGDAKmb28v1NLXA@cluster0.eo4vmzk.mongodb.net/?retryWrites=true&w=majority',
    { useNewUrlParser: true, 
@@ -20,38 +21,6 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/books', (req, res, next) => {
-   delete req.body._id;
-   const book = new Book({
-     ...req.body
-   });
-   book.save()
-     .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
-     .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/books/:id', (req, res, next) => {
-   Book.findOne({ _id: req.params.id })
-     .then(book => res.status(200).json(book))
-     .catch(error => res.status(404).json({ error }));
-});
-
-app.use('/api/books', (req, res, next) => {
-   Book.find()
-     .then(books => res.status(200).json(books))
-     .catch(error => res.status(400).json({ error }));
-});
-
-app.put('/api/books/:id', (req, res, next) => {
-   Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-   .then(() => res.status(200).json({ message: 'Livre modifié !'}))
-   .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/books/:id', (req, res, next) => {
-   Book.deleteOne({ _id: req.params.id })
-   .then(() => res.status(200).json({ message: 'Livre supprimé !'}))
-   .catch(error => res.status(400).json({ error }));
-});
+app.use('/api/books', bookRoutes);
 
 module.exports = app;
